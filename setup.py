f29ca0683619d45f011a7e6261a4405d1a30d688
Alexander Chambers <alexander.chambers8427@gmail.com>
2018-05-22 13:30:46 +0930

    Remove automated testing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='simtables',
-    version='0.0.2',
+    version='0.0.1',
     author='Alexander Chambers',
     author_email='alexander.chambers8472@gmail.com',
     description='Simple tables',
@@ -18,7 +18,7 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ),
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
-    test_suite='tests',
+    # setup_requires=['pytest-runner'],
+    # tests_require=['pytest'],
+    # test_suite='tests',
 )
